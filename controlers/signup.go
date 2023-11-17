ccd86a6ecdf9d972e9db7d3ba11e615de0f434c9
Lordkaito <cespedesisai88@gmail.com>
2023-11-17 19:15:20 +0100

    Update signup endpoing. Fix problem with response when creating a user

--- controlers/signup.go
+++ controlers/signup.go
@@ -8,7 +8,29 @@ import (
 
 type Response struct {
 	Message string `json:"message"`
-	User    string `json:"user,omitempty"`
+	User    User   `json:"user,omitempty"`
+}
+
+type Post struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	ID      string `json:"id"`
+	Author  string `json:"author"`
+	User_id string `json:"user_id"`
+	Likes   []Like `json:"likes"`
+}
+
+type Like struct {
+	Post_id string `json:"post_id"`
+	User_id string `json:"user_id"`
+	ID      string `json:"id"`
+}
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	ID    int    `json:"id"`
+	Posts []Post `json:"posts"`
 }
 
 func SignupHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,24 +45,40 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	req.Header.Set("Content-Type", "application/json")
 
 	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(err.Error()))
+		return
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Println(err)
 		w.Write([]byte(err.Error()))
+		return
 	}
 
 	defer resp.Body.Close()
 
 	var response Response
 
-	jsonResponse, err := json.Marshal(response)
+	err = json.NewDecoder(resp.Body).Decode(&response)
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
 		w.Write([]byte(err.Error()))
+		return
+	}
+
+	jsonResponse, err := json.Marshal(response)
+	fmt.Println("Response:", response)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusCreated)
 	w.Write(jsonResponse)
 }
